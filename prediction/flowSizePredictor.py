bf2e14fc8a2dfeaa14070de91bfea79906d8a9a5
Isadora <isadorapossebon@gmail.com>
2019-09-04 16:14:15 -0300

    Add unicauca dataset

--- prediction/flowSizePredictor.py
+++ prediction/flowSizePredictor.py
@@ -1,4 +1,6 @@
 import csv
+import pandas as pd
+import numpy as np
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
@@ -11,10 +13,40 @@ class FlowSizePredictor(object):
         self.training_set = []
 
     def trainModel(self):
+        dataset_path = './datasets/unicauca1000.csv'
+
+        raw_dataset = pd.read_csv(
+            dataset_path,
+            na_values = "?",
+            comment='\t',
+            sep=",",
+            skipinitialspace=True
+        )
+        dataset = raw_dataset.copy()
+
+        # remove colunas com valores irrelevantes: parâmetro 1 é para colunas, 0 é para linhas
+        # dataset = dataset.drop('Flow.ID', 1)
+        # dataset = dataset.drop('Source.IP', 1)
+        # dataset = dataset.drop('Source.Port', 1) # infos qualitativas
+        # dataset = dataset.drop('Destination.IP', 1) # infos qualitativas
+        # dataset = dataset.drop('Destination.Port', 1) # infos qualitativas
+        # dataset = dataset.drop('Protocol', 1) # infos qualitativas
+        # dataset = dataset.drop('Timestamp', 1) # infos qualitativas
+        # dataset = dataset.drop('Label', 1) # infos qualitativas
+        # dataset = dataset.drop('L7Protocol', 1) # infos qualitativas
+        # dataset = dataset.drop('ProtocolName', 1) # infos qualitativas
+        #
+        # dataset = dataset.drop('Flow.Duration', 1) # não vamos ter essa feature
+        #
+        # # drop rows that contain unknown values
+        # dataset = dataset.dropna()
+
+        dataset = dataset[['Flow.Duration', 'Total.Fwd.Packets', 'Total.Length.of.Fwd.Packets']]
+
         # Split dataset into a training set and a test set
-        train_dataset = self.training_set.sample(frac=0.8,random_state=0)
-        test_dataset = self.training_set.drop(train_dataset.index)
-        features = list(self.training_set.columns.values)
+        train_dataset = dataset.sample(frac=0.8,random_state=0)
+        test_dataset = dataset.drop(train_dataset.index)
+        features = list(dataset.columns.values)
 
         train_dataset_features = train_dataset.loc[:, features].values.tolist()
         train_dataset_correct_value = train_dataset.loc[:,'Total.Length.of.Fwd.Packets'].tolist()

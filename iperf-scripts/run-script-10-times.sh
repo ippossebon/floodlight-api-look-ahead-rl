c1029a1acc73185b6bdc38ffc21a05796e35f2ce
Isadora <isadorapossebon@gmail.com>
2019-09-17 22:09:01 -0300

    Add script to run iperf scripts 10 times

---
+++ iperf-scripts/run-script-10-times.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "Início execução 1";
+sh ./iperf-script-h1-server.sh > outuput_exec_1.txt;
+echo "Fim execução 1";
+
+echo "Início execução 2";
+sh ./iperf-script-h1-server.sh > outuput_exec_2.txt;
+echo "Fim execução 2";
+
+echo "Início execução 3";
+sh ./iperf-script-h1-server.sh > outuput_exec_3.txt;
+echo "Fim execução 3";
+
+echo "Início execução 4";
+sh ./iperf-script-h1-server.sh > outuput_exec_4.txt;
+echo "Fim execução 4";
+
+echo "Início execução 5";
+sh ./iperf-script-h1-server.sh > outuput_exec_5.txt;
+echo "Fim execução 5";
+
+echo "Início execução 6";
+sh ./iperf-script-h1-server.sh > outuput_exec_6.txt;
+echo "Fim execução 6";
+
+echo "Início execução 7";
+sh ./iperf-script-h1-server.sh > outuput_exec_7.txt;
+echo "Fim execução 7";
+
+echo "Início execução 8";
+sh ./iperf-script-h1-server.sh > outuput_exec_8.txt;
+echo "Fim execução 8";
+
+echo "Início execução 9";
+sh ./iperf-script-h1-server.sh > outuput_exec_9.txt;
+echo "Fim execução 9";
+
+echo "Início execução 10";
+sh ./iperf-script-h1-server.sh > outuput_exec_10.txt;
+echo "Fim execução 10";

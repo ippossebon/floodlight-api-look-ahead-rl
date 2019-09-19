0c29bc75fe3e06ad99e988167f34af1d7e7122fc
Isadora <isadorapossebon@gmail.com>
2019-09-19 07:45:10 -0300

    change output file name

--- iperf-scripts/run-script-10-times.sh
+++ iperf-scripts/run-script-10-times.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 echo "Inicio execucao 1";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_1.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_1.txt;
 echo "Fim execucao 1";
 
 echo "Inicio execucao 2";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_2.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_2.txt;
 echo "Fim execucao 2";
 
 echo "Inicio execucao 3";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_3.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_3.txt;
 echo "Fim execucao 3";
 
 echo "Inicio execucao 4";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_4.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_4.txt;
 echo "Fim execucao 4";
 
 echo "Inicio execucao 5";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_5.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_5.txt;
 echo "Fim execucao 5";
 
 echo "Inicio execucao 6";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_6.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_6.txt;
 echo "Fim execucao 6";
 
 echo "Inicio execucao 7";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_7.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_7.txt;
 echo "Fim execucao 7";
 
 echo "Inicio execucao 8";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_8.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_8.txt;
 echo "Fim execucao 8";
 
 echo "Inicio execucao 9";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_9.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_9.txt;
 echo "Fim execucao 9";
 
 echo "Inicio execucao 10";
-sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_10.txt;
+sh ./iperf-script-h2-server.sh >> outuput_exec_10.txt;
 echo "Fim execucao 10";

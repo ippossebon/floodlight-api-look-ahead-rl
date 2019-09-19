8621f0660685a5280599e2c67572fd08c06c6b77
Isadora <isadorapossebon@gmail.com>
2019-09-19 07:42:48 -0300

    Change scripts to run with h2 as server

--- iperf-scripts/run-script-10-times.sh
+++ iperf-scripts/run-script-10-times.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
-echo "Início execução 1";
-sh ./iperf-script-h1-server.sh >> outuput_exec_1.txt;
-echo "Fim execução 1";
+echo "Inicio execucao 1";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_1.txt;
+echo "Fim execucao 1";
 
-echo "Início execução 2";
-sh ./iperf-script-h1-server.sh >> outuput_exec_2.txt;
-echo "Fim execução 2";
+echo "Inicio execucao 2";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_2.txt;
+echo "Fim execucao 2";
 
-echo "Início execução 3";
-sh ./iperf-script-h1-server.sh >> outuput_exec_3.txt;
-echo "Fim execução 3";
+echo "Inicio execucao 3";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_3.txt;
+echo "Fim execucao 3";
 
-echo "Início execução 4";
-sh ./iperf-script-h1-server.sh >> outuput_exec_4.txt;
-echo "Fim execução 4";
+echo "Inicio execucao 4";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_4.txt;
+echo "Fim execucao 4";
 
-echo "Início execução 5";
-sh ./iperf-script-h1-server.sh >> outuput_exec_5.txt;
-echo "Fim execução 5";
+echo "Inicio execucao 5";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_5.txt;
+echo "Fim execucao 5";
 
-echo "Início execução 6";
-sh ./iperf-script-h1-server.sh >> outuput_exec_6.txt;
-echo "Fim execução 6";
+echo "Inicio execucao 6";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_6.txt;
+echo "Fim execucao 6";
 
-echo "Início execução 7";
-sh ./iperf-script-h1-server.sh >> outuput_exec_7.txt;
-echo "Fim execução 7";
+echo "Inicio execucao 7";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_7.txt;
+echo "Fim execucao 7";
 
-echo "Início execução 8";
-sh ./iperf-script-h1-server.sh >> outuput_exec_8.txt;
-echo "Fim execução 8";
+echo "Inicio execucao 8";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_8.txt;
+echo "Fim execucao 8";
 
-echo "Início execução 9";
-sh ./iperf-script-h1-server.sh >> outuput_exec_9.txt;
-echo "Fim execução 9";
+echo "Inicio execucao 9";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_9.txt;
+echo "Fim execucao 9";
 
-echo "Início execução 10";
-sh ./iperf-script-h1-server.sh >> outuput_exec_10.txt;
-echo "Fim execução 10";
+echo "Inicio execucao 10";
+sh ./iperf-script-h2-server.sh >> ./h2-as-server/outuput_exec_10.txt;
+echo "Fim execucao 10";

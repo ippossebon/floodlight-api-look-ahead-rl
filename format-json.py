2667101380e146f1703da0011aa518e6f4844054
Isadora <isadorapossebon@gmail.com>
2019-10-01 06:45:19 -0300

    add formated csv file for snapshots from 30-set-madrugada

--- format-json.py
+++ format-json.py
@@ -1,8 +1,8 @@
 import json
 
 data = {}
-with open('./snapshots-json-big-file.txt', 'r') as infile, \
-     open('./snapshots-json-big-file-formatted.txt', 'w+') as outfile:
+with open('./snapshots-madrugada-30-set--fallback.txt', 'r') as infile, \
+     open('./snapshots-madrugada-30-set--fallback-formatted.txt', 'w+') as outfile:
     data = infile.read()
     data = data.replace("\\", "")
     outfile.write(data)

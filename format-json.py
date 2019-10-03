2d7fe2babd4cc190fac090653bef8f5337cc6c8b
Isadora <isadorapossebon@gmail.com>
2019-10-03 06:15:54 -0300

    Add h3 server snapshots formatted files

--- format-json.py
+++ format-json.py
@@ -1,8 +1,8 @@
 import json
 
 data = {}
-with open('./snapshots/h2-as-server/snapshot-h4-client-h2-server--fallback.txt', 'r') as infile, \
-     open('./snapshots/h2-as-server/snapshot-h4-client-h2-server--fallback-formatted.txt', 'w+') as outfile:
+with open('./snapshots/h3-as-server/snapshot-h4-client-h3-server--fallback.txt', 'r') as infile, \
+     open('./snapshots/h3-as-server/snapshot-h4-client-h3-server--fallback-formatted.txt', 'w+') as outfile:
     data = infile.read()
     data = data.replace("\\", "")
     outfile.write(data)

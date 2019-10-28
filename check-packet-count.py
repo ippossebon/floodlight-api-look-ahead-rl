5bf91373e5eea8e92ce4e010f729952d9aab64a8
Isadora <isadorapossebon@gmail.com>
2019-10-27 23:26:16 -0300

    Remove unused files

--- check-packet-count.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-filename = './snapshots/csv/snapshots-json-big-file--client.csv'
-
-with open(filename, 'r+') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=';')
-
-    for row in readCSV:
-        print(row[8])
--- prediction/test.py
+++
@@ -1,6 +0,0 @@
-from .flowSizePredictor import FlowSizePredictor
-
-if __name__ == '__main__':
-    predictor = FlowSizePredictor()
-
-    predictor.trainModel()

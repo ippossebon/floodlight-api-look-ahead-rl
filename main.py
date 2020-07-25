9160a010f4eb38db6b360633a2dd0db5450ff870
Isadora <isadorapossebon@gmail.com>
2020-07-24 21:43:01 -0300

    Fix testing code errors

--- main.py
+++ main.py
@@ -228,8 +228,8 @@ class LookAheadRLApp(object):
                         # Atualiza rotas pelas quais passa - variável auxiliar
                         in_port = flow['match']['in_port']
                         out_port_plain_text = flow['instructions']['instruction_apply_actions']['actions']
-                        print('isadora', re.match('\d+', out_port_plain_text))
-                        out_port = float(re.match('\d+', out_port_plain_text))
+                        print('isadora', re.findall(r"\d+", out_port_plain_text))
+                        out_port = float(re.findall(r"\d+", out_port_plain_text)[0])
 
                         link = rulesToLink(switch_address, out_port)
                         flow_paths[flow_id].append(link)

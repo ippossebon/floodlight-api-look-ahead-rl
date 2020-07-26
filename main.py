6e63e102930ed9b849dc8eb82465fcef6627e429
Isadora <isadorapossebon@gmail.com>
2020-07-26 20:13:39 -0300

    Fix testing code errors

--- main.py
+++ main.py
@@ -204,9 +204,6 @@ class LookAheadRLApp(object):
             flow_paths[active_flow_id] = [] # precisa considerar self.active_flows_paths
             flow_size[active_flow_id] = []  # precisa considerar self.active_flows_size
 
-        print('(1) Flows paths LOCAL: ', flow_paths)
-        print('(1) Flows size LOCAL: ', flow_size)
-
         for switch_address in response_data:
             for flow in response_data[switch_address]['flows']:
                 contains_match = len(flow['match'].keys()) > 1
@@ -225,8 +222,6 @@ class LookAheadRLApp(object):
 
                         # Se não existir na lista de fluxos ativos, adiciona
                         if flow_id not in self.active_flows_id:
-                            print('* flow novo')
-
                             self.addActiveFlow(flow_id)
 
                             # Atualiza rotas pelas quais passa - variável auxiliar
@@ -234,8 +229,6 @@ class LookAheadRLApp(object):
 
                             # Atualiza tamanho do fluxo por link - variável auxiliar
                             flow_size[flow_id] = []
-                        else:
-                            print('* flow existia')
 
                         # Atualiza rotas pelas quais passa - variável auxiliar
                         in_port = flow['match']['in_port']
@@ -244,11 +237,8 @@ class LookAheadRLApp(object):
 
                         link = rulesToLink(switch_address, out_port)
                         print('switch_address = {0}, out_port = {1}, link = {2}'.format(switch_address, out_port, link))
-                        print('(2) Flows paths LOCAL: ', flow_paths)
-                        print('(2) Flows size LOCAL: ', flow_size)
-
-                        print('flow_id in flow_paths: ', flow_id in flow_paths.keys())
-                        print('Flows flow_paths[flow_id]: ', flow_paths[flow_id])
+                        print('Flows paths LOCAL: ', flow_paths)
+                        print('Flows size LOCAL: ', flow_size)
 
                         if link:
                             flow_paths[flow_id].append(link) # e quando tiver mais de um caminho??
@@ -259,6 +249,11 @@ class LookAheadRLApp(object):
                             if flow_id not in flow_ids_to_update:
                                 flow_ids_to_update.append(flow_id)
 
+
+        print('* Final do step *')
+        print('flow_ids_to_update = ', flow_ids_to_update)
+        print('flow_paths = ', flow_paths)
+        print('flow_size = ', flow_size)
 
         for flow_id in flow_ids_to_update:
             self.updateFlowPaths(flow_id, flow_paths[flow_id])

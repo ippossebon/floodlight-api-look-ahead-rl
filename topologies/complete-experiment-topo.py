9978b3a0e2d142d1b564beb7604db601cb235d3b
Isadora <isadorapossebon@gmail.com>
2020-08-02 18:09:17 -0300

    Fix testing code errors

--- topologies/complete-experiment-topo.py
+++ topologies/complete-experiment-topo.py
@@ -6,6 +6,7 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.log import setLogLevel
 from mininet.cli import CLI
+from mininet.link import TCLink
 from mininet.node import OVSSwitch, Controller, RemoteController
 
 class MastersSwitchTopo(Topo):
@@ -25,14 +26,16 @@ class MastersSwitchTopo(Topo):
  	        node2 = s1,
  	        port1 = 1,
  	        port2 = 1,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = h2,
  	        node2 = s3,
  	        port1 = 1,
  	        port2 = 1,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
 
         # Adiciona links entre os switches
@@ -41,49 +44,56 @@ class MastersSwitchTopo(Topo):
             node2 = s2,
             port1 = 2,
             port2 = 1,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = s1,
             node2 = s4,
             port1 = 3,
             port2 = 1,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = s2,
             node2 = s4,
             port1 = 2,
             port2 = 2,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = s2,
             node2 = s5,
             port1 = 3,
             port2 = 1,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = s2,
             node2 = s3,
             port1 = 4,
             port2 = 2,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = s4,
             node2 = s3,
             port1 = 3,
             port2 = 3,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
         self.addLink(
             node1 = s5,
             node2 = s3,
             port1 = 2,
             port2 = 4,
-            mbps=10 # bw = Mbits/seg
+            mbps=10, # bw = Mbits/seg,
+            cls=TCLink
         )
 
 if __name__ == '__main__':

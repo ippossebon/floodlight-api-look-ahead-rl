d2580606ba2af3a102aa317b8034e2483a7b70f8
Isadora <isadorapossebon@gmail.com>
2020-08-21 22:11:30 -0300

    add logging

--- load_balance_gym/envs/teste-env-floodlight.py
+++ load_balance_gym/envs/teste-env-floodlight.py
@@ -1,4 +1,5 @@
 from load_balance_with_floodlight import LoadBalanceEnv
 
 env = LoadBalanceEnv(num_flows = 10, source_port = 1, source_switch = 0, target_port = 1, target_switch = 2)
+env.getState()
 print('Feito.')

7475e686bcb2b4b263912e76bae1bf6d7925433a
Isadora <isadorapossebon@gmail.com>
2020-08-24 20:47:41 -0300

    add logging

--- load_balance_gym/envs/teste-env-floodlight.py
+++ load_balance_gym/envs/teste-env-floodlight.py
@@ -1,6 +1,8 @@
 from load_balance_with_floodlight import LoadBalanceEnv
 from staticFlowPusher import StaticFlowPusher
 
+CONTROLLER_IP = 'http://0.0.0.0'
+
 # Fluxo sai de H1 e vai para H2
 env = LoadBalanceEnv(num_flows = 10, source_port = 1, source_switch = 0, target_port = 1, target_switch = 2)
 pusher = StaticFlowPusher(CONTROLLER_IP)

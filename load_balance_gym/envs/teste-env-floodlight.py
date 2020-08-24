20c8476072d6a3f7774ef6e180d290d7f1857e3e
Isadora <isadorapossebon@gmail.com>
2020-08-24 20:20:29 -0300

    add logging

--- load_balance_gym/envs/teste-env-floodlight.py
+++ load_balance_gym/envs/teste-env-floodlight.py
@@ -1,6 +1,8 @@
 from load_balance_with_floodlight import LoadBalanceEnv
 
+# Fluxo sai de H1 e vai para H2
 env = LoadBalanceEnv(num_flows = 10, source_port = 1, source_switch = 0, target_port = 1, target_switch = 2)
 state = env.getState()
 print('state = ', state)
+
 print('Feito.')

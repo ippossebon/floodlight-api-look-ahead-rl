6ec05ee8426f44f2f2d18b587d6ad650f9a7d735
Isadora <isadorapossebon@gmail.com>
2020-08-24 21:08:07 -0300

    testing static flow pusher

--- load_balance_gym/envs/teste-env-floodlight.py
+++ load_balance_gym/envs/teste-env-floodlight.py
@@ -2,12 +2,37 @@ from load_balance_with_floodlight import LoadBalanceEnv
 from staticFlowPusher import StaticFlowPusher
 
 import time
-
+import http.client, urllib.parse
 
 CONTROLLER_IP = 'http://0.0.0.0'
 
 # Fluxo sai de H1 e vai para H2
 env = LoadBalanceEnv(num_flows = 10, source_port = 1, source_switch = 0, target_port = 1, target_switch = 2)
+
+flow_1_params = urllib.parse.urlencode({
+    'switch':'00:00:00:00:00:00:00:01',
+        'name':'flow_1',
+        'cookie':'0',
+        'priority':'32768',
+        'in_port':'1',
+        'active':'true',
+        'actions':'output=2'
+})
+headers = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+}
+conn = http.client.HTTPConnection(CONTROLLER_IP)
+conn.request('POST', '', params, headers)
+response = conn.getresponse()
+
+print('Status = ', response.status )
+print('Reason = ', response.reason)
+data = response.read()
+
+print('Data = ', data)
+
+conn.close()
 
 # print('vai criar o pusher')
 # pusher = StaticFlowPusher(CONTROLLER_IP)

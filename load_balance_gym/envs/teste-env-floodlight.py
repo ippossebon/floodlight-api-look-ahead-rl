5237b46057b577f7ddff582279bd8eea55688359
Isadora <isadorapossebon@gmail.com>
2020-08-24 21:00:29 -0300

    add logging

--- load_balance_gym/envs/teste-env-floodlight.py
+++ load_balance_gym/envs/teste-env-floodlight.py
@@ -6,34 +6,34 @@ CONTROLLER_IP = 'http://0.0.0.0'
 # Fluxo sai de H1 e vai para H2
 env = LoadBalanceEnv(num_flows = 10, source_port = 1, source_switch = 0, target_port = 1, target_switch = 2)
 
-print('vai criar o pusher')
-pusher = StaticFlowPusher(CONTROLLER_IP)
-print('criou o pusher')
-
-
-flow1 = {
-    'switch':'00:00:00:00:00:00:00:01',
-    'name':'flow_1',
-    'cookie':'0',
-    'priority':'32768',
-    'in_port':'1',
-    'active':'true',
-    'actions':'output=2'
-    }
-
-flow2 = {
-    'switch':'00:00:00:00:00:00:00:01',
-    'name':'flow_2',
-    'cookie':'0',
-    'priority':'32768',
-    'in_port':'1',
-    'active':'true',
-    'actions':'output=2'
-    }
-
-pusher.set(flow1)
-pusher.set(flow2)
-print('Adicionou fluxos.')
+# print('vai criar o pusher')
+# pusher = StaticFlowPusher(CONTROLLER_IP)
+# print('criou o pusher')
+#
+#
+# flow1 = {
+#     'switch':'00:00:00:00:00:00:00:01',
+#     'name':'flow_1',
+#     'cookie':'0',
+#     'priority':'32768',
+#     'in_port':'1',
+#     'active':'true',
+#     'actions':'output=2'
+#     }
+#
+# flow2 = {
+#     'switch':'00:00:00:00:00:00:00:01',
+#     'name':'flow_2',
+#     'cookie':'0',
+#     'priority':'32768',
+#     'in_port':'1',
+#     'active':'true',
+#     'actions':'output=2'
+#     }
+#
+# pusher.set(flow1)
+# pusher.set(flow2)
+# print('Adicionou fluxos.')
 
 sleep(4)
 

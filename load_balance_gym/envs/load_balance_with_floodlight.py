05dcae3c05d31831784b18decf093321bd4124df
Isadora <isadorapossebon@gmail.com>
2020-08-27 09:08:21 -0300

    add logging

--- load_balance_gym/envs/load_balance_with_floodlight.py
+++ load_balance_gym/envs/load_balance_with_floodlight.py
@@ -366,13 +366,7 @@ class LoadBalanceEnv(gym.Env):
         }
 
         """
-        print('port = ', port)
-        print('port type = ', type(port))
-
         for possible_port in self.switch_possible_ports[switch_id]:
-            print('possible_port = ', possible_port)
-            print('possible_port type = ', type(possible_port))
-
             if str(possible_port) == port:
                 return True
 

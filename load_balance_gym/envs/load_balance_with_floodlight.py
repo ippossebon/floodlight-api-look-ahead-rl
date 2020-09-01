f5f30f95b449a4104694850ed8e8f23af83c66a4
Isadora <isadorapossebon@gmail.com>
2020-08-31 21:39:33 -0300

    add logging

--- load_balance_gym/envs/load_balance_with_floodlight.py
+++ load_balance_gym/envs/load_balance_with_floodlight.py
@@ -371,6 +371,8 @@ class LoadBalanceEnv(gym.Env):
         response = requests.get('{host}/wm/core/switch/all/flow/json'.format(host=CONTROLLER_HOST))
         response_data = response.json()
 
+        print('response de flows: ', response_data)
+
         max_byte_count = -1
         max_usage_flow_id = None # preciso de um fallback
 

6e33a3a76caf43c93aafac3951da4eb834e961ec
Isadora <isadorapossebon@gmail.com>
2020-09-23 20:17:46 -0300

    fix coding errors

--- iperf-scripts/simultaneous-iperfs.sh
+++ iperf-scripts/simultaneous-iperfs.sh
@@ -1,4 +1,9 @@
+iperf -c 10.0.0.2 -n 100G &
+iperf -c 10.0.0.2 -n 200G &
+iperf -c 10.0.0.2 -n 50G &
 iperf -c 10.0.0.2 -n 800G &
-iperf -c 10.0.0.2 -n 800G &
-iperf -c 10.0.0.2 -n 800G &
-iperf -c 10.0.0.2 -n 800G &
+iperf -c 10.0.0.2 -n 150G &
+iperf -c 10.0.0.2 -n 200G &
+iperf -c 10.0.0.2 -n 100G &
+iperf -c 10.0.0.2 -n 400G &
+iperf -c 10.0.0.2 -n 80G &

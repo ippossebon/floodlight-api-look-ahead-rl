c5e5e54d52f422fd07c70a61b1fc092d0349a6b8
Isadora <isadorapossebon@gmail.com>
2020-09-24 21:22:33 -0300

    add debugging prints

--- run-experiments.py
+++ run-experiments.py
@@ -107,19 +107,19 @@ def startEnv():
 def trainAgent(env):
     # Parametros adicionais para criar o modelo: gamma (discount_factor), n_steps (numero de steps para rodar para cada env por update), learning_rate
     model = A2C(policy=MlpPolicy, env=env, verbose=1, learning_rate=0.05, gamma=0.97)
-    model.learn(total_timesteps=10000)
-    model.save('./A2C_10000_lr_005_gamma_097-disc-env')
+    model.learn(total_timesteps=100)
+    model.save('./A2C_100_lr_005_gamma_097-disc-env')
     print('Modelo treinado e salvo.')
 
 
 def testAgent(env):
     print('Testando o agente...')
-    model = A2C.load(load_path='./A2C_10000_lr_005_gamma_097-disc-env', env=env)
-    # model = A2C.load(load_path='./A2C_10000_lr_005_gamma_097-disc-env-sem_acao_inv', env=env)
+    model = A2C.load(load_path='./A2C_100_lr_005_gamma_097-disc-env', env=env)
+    # model = A2C.load(load_path='./A2C_100_lr_005_gamma_097-disc-env-sem_acao_inv', env=env)
 
 
     state = env.reset()
-    num_steps = 1000
+    num_steps = 30
 
     print('State: ', state)
 
@@ -141,7 +141,7 @@ def testAgent(env):
 
 def runExperiments():
     print('Rodando experimentos...')
-    model = A2C.load(load_path='./A2C_10000_lr_005_gamma_097-disc-env', env=env)
+    model = A2C.load(load_path='./A2C_100_lr_005_gamma_097-disc-env', env=env)
     env.reset()
     update_count = 0
 
@@ -208,7 +208,7 @@ def plotGraphs(p0, p1, p2, p3, p4, p5, p6, p7, p8,p9, p10, p11, p12, p13, p14, p
     # show a legend on the plot
     plt.legend()
 
-    plt.savefig('A2C_10000-100_steps-005-097-23_set.pdf')
+    plt.savefig('A2C_100-100_steps-005-097-23_set.pdf')
 
     print('Grafico gerado')
 

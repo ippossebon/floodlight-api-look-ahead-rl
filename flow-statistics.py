6dfa7961ba6daa5740e8e3aca6bc533a69d56a19
Isadora <isadorapossebon@gmail.com>
2020-09-26 19:09:55 -0300

    Add flow statistics script

---
+++ flow-statistics.py
@@ -0,0 +1,15 @@
+import requests
+
+
+while True:
+    response = requests.get('http://localhost:8080/wm/core/switch/all/flow/json')
+    response_data = response.json()
+
+    switch_ids = sort(response_data.keys())
+    statistics = {}
+
+    for switch_id in switch_ids:
+        num_flows = len(response_data[switch_id]['flows'])
+        statistics[switch_id] = num_flows
+
+    print(statistics)

e9956a9618d1ade14f90e0cb9b46117a2bb1df26
Isadora <isadorapossebon@gmail.com>
2020-10-01 16:28:00 -0300

    add debugging prints

--- load_balance_gym/envs/load_balance_with_floodlight_disc_action.py
+++ load_balance_gym/envs/load_balance_with_floodlight_disc_action.py
@@ -394,7 +394,7 @@ class LoadBalanceEnvDiscAction(gym.Env):
 
         for i in range(len(statistics_tx)):
             if statistics_tx[i] > self.previous_tx[i]:
-                mbits_transfered = (statistics_tx[i] - self.previous_tx[i]) / (1024 * 1024) # valor em Mbits
+                mbits_transfered = (statistics_tx[i] - self.previous_tx[i]) #/ (1024 * 1024) # valor em Mbits
                 state[i] = mbits_transfered / diff_seconds
             else:
                 state[i] = self.prev_state[i]

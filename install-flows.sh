64622b38f17079e4eca0a617828717132ab420f3
Isadora <isadorapossebon@gmail.com>
2020-10-07 14:03:59 -0300

    fix flows script

--- install-flows.sh
+++ install-flows.sh
@@ -9,14 +9,14 @@ for client_port in ${ClientPortsArray[@]}; do
   entry1 = $client_port
   curl -X POST http://localhost:8080//wm/staticentrypusher/json -d '{
         "switch": "00:00:00:00:00:00:00:01",
-        "name": "s1-flow-$client_port"
+        "name": "'s1-flow-$client_port'"
         "priority": "0",
         "in_port": "1",
         "active": "true",
         "eth_type": "0x0800",
         "ipv4_src": "10.0.0.1",
         "ipv4_dst": "10.0.0.2",
-        "tcp_src": "$client_port",
+        "tcp_src": "'$client_port'",
         "tcp_dst": "5201",
         "actions": "output=2"
     }'
@@ -24,14 +24,14 @@ for client_port in ${ClientPortsArray[@]}; do
     # Flow entries on S2
     curl -X POST http://localhost:8080//wm/staticentrypusher/json -d '{
           switch": "00:00:00:00:00:00:00:02",
-          "name": "s2-flow-$client_port",
+          "name": "'s2-flow-$client_port'",
           "priority": "0",
           "in_port": "1",
           "active": "true",
           "eth_type": "0x0800",
           "ipv4_src": "10.0.0.1",
           "ipv4_dst": "10.0.0.2",
-          "tcp_src": "$client_port",
+          "tcp_src": "'$client_port'",
           "tcp_dst": "5201",
           "actions": "output=4"
       }'
@@ -39,14 +39,14 @@ for client_port in ${ClientPortsArray[@]}; do
       # Flow entries on S3
       curl -X POST http://localhost:8080//wm/staticentrypusher/json -d '{
             "switch": "00:00:00:00:00:00:00:03",
-            "name": "s3-flow-$client_port",
+            "name": "'s3-flow-$client_port'",
             "priority": "0",
             "in_port": "2",
             "active": "true",
             "eth_type": "0x0800",
             "ipv4_src": "10.0.0.1",
             "ipv4_dst": "10.0.0.2",
-            "tcp_src": "$client_port",
+            "tcp_src": "'$client_port'",
             "tcp_dst": "5201",
             "actions": "output=1"
       }'

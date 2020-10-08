70ca2b2999e76489558c34e8cc403cd17c05c30a
Isadora <isadorapossebon@gmail.com>
2020-10-08 20:45:41 -0300

    add s5-h5 topology

--- install-flows-verbose.sh
+++ install-flows-verbose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # declare -a ClientPortsArray=("46110" "46112" "46114" "46116" "46118" "46120" "46122" "46124" "46126" "46128")
-curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
+curl -X POST http://192.168.68.250:8080/wm/staticflowpusher/json -d '{
       "switch": "00:00:00:00:00:00:00:01",
       "name": "s1-flow-46110-to-5201",
       "priority": "0",
@@ -10,27 +10,26 @@ curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
       "eth_type": "0x0800",
       "ipv4_src": "10.0.0.1",
       "ipv4_dst": "10.0.0.2",
-      "tcp_src": "46110",
-      "tcp_dst": "5201",
+      "ip_proto": "0x06",
       "actions": "output=2"
   }'
 
-curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
+curl -X POST http://192.168.68.250:8080/wm/staticflowpusher/json -d '{
     "switch": "00:00:00:00:00:00:00:01",
     "name": "s1-flow-5201-to-46110",
     "priority": "0",
     "in_port": "1",
     "active": "true",
     "eth_type": "0x0800",
-    "ipv4_src": "10.0.0.1",
-    "ipv4_dst": "10.0.0.2",
-    "tcp_dst": "46110",
-    "tcp_src": "5201",
+    "ipv4_dst": "10.0.0.1",
+    "ipv4_src": "10.0.0.2",
+    "tcp_dst": 46110,
+    "tcp_src": 5201,
     "actions": "output=2"
 }'
 
 # Flow entries on S2
-curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
+curl -X POST http://192.168.68.250:8080/wm/staticflowpusher/json -d '{
     switch": "00:00:00:00:00:00:00:02",
     "name": "s2-flow-46110",
     "priority": "0",
@@ -44,7 +43,7 @@ curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
     "actions": "output=4"
 }'
 
-curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
+curl -X POST http://192.168.68.250:8080/wm/staticflowpusher/json -d '{
       switch": "00:00:00:00:00:00:00:02",
       "name": "s2-flow-5201-to-46110",
       "priority": "0",
@@ -59,7 +58,7 @@ curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
   }'
 
 # Flow entries on S3
-curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
+curl -X POST http://192.168.68.250:8080/wm/staticflowpusher/json -d '{
       "switch": "00:00:00:00:00:00:00:03",
       "name": "s3-flow-46110",
       "priority": "0",
@@ -73,7 +72,7 @@ curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
       "actions": "output=1"
 }'
 
-curl -X POST http://192.168.68.118:8080/wm/staticflowpusher/json -d '{
+curl -X POST http://192.168.68.250:8080/wm/staticflowpusher/json -d '{
       "switch": "00:00:00:00:00:00:00:03",
       "name": "s3-flow-5201-to-46110",
       "priority": "0",
---
+++ topologies/s5-h5.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.node import OVSSwitch, Controller, RemoteController
+
+class MastersSwitchTopo(Topo):
+    def build(self):
+        s1 = self.addSwitch('S1', mac="00:00:00:00:00:01", protocols='OpenFlow13')
+        s2 = self.addSwitch('S2', mac="00:00:00:00:00:02", protocols='OpenFlow13')
+        s3 = self.addSwitch('S3', mac="00:00:00:00:00:03", protocols='OpenFlow13')
+        s4 = self.addSwitch('S4', mac="00:00:00:00:00:04", protocols='OpenFlow13')
+        s5 = self.addSwitch('S5', mac="00:00:00:00:00:05", protocols='OpenFlow13')
+
+        h1 = self.addHost('H1', mac="00:00:00:00:00:11", ip="10.0.0.1/12")
+        h2 = self.addHost('H2', mac="00:00:00:00:00:12", ip="10.0.0.2/12")
+        h3 = self.addHost('H3', mac="00:00:00:00:00:13", ip="10.0.0.3/12")
+        h4 = self.addHost('H4', mac="00:00:00:00:00:14", ip="10.0.0.4/12")
+        h5 = self.addHost('H5', mac="00:00:00:00:00:15", ip="10.0.0.5/12")
+
+
+        # Adiciona hosts aos switches
+        self.addLink(
+            node1 = h1,
+ 	        node2 = s1,
+ 	        port1 = 1,
+ 	        port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = h2,
+ 	        node2 = s3,
+ 	        port1 = 1,
+ 	        port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = h3,
+ 	        node2 = s1,
+ 	        port1 = 1,
+ 	        port2 = 3,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = h4,
+ 	        node2 = s1,
+ 	        port1 = 1,
+ 	        port2 = 4,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = h5,
+ 	        node2 = s1,
+ 	        port1 = 1,
+ 	        port2 = 5,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        # Adiciona links entre os switches
+        self.addLink(
+            node1 = s1,
+            node2 = s2,
+            port1 = 2,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = s1,
+            node2 = s4,
+            port1 = 6,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = s2,
+            node2 = s4,
+            port1 = 2,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = s2,
+            node2 = s5,
+            port1 = 3,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = s2,
+            node2 = s3,
+            port1 = 4,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = s4,
+            node2 = s3,
+            port1 = 3,
+            port2 = 3,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+        self.addLink(
+            node1 = s5,
+            node2 = s3,
+            port1 = 2,
+            port2 = 4,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topo = MastersSwitchTopo()
+    # c1 = RemoteController('c1', ip='127.0.0.1') #usando RYU
+    # c1 = RemoteController('c1', ip='0.0.0.0', port=6653) #usando Floodlight
+    c1 = RemoteController('c1', ip='localhost', port=6653) #usando Floodlight
+
+    net = Mininet(topo=topo, controller=c1)
+    net.start()
+    #net.pingAll()
+    CLI(net)
+    net.stop()

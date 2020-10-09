6095798c8df0c1756c42e963a9aebd012211d297
Isadora <isadorapossebon@gmail.com>
2020-10-08 22:28:56 -0300

    wip

--- run-experiments.py
+++ run-experiments.py
@@ -370,7 +370,7 @@ def run():
     # print('Add initial entries')
     # addInitialEntries()
     #
-    # env = LoadBalanceEnvDiscAction(source_port_index=0, source_switch_index=0, target_port_index=0, target_switch_index=2)
+    env = LoadBalanceEnvDiscAction(source_port_index=0, source_switch_index=0, target_port_index=0, target_switch_index=2)
     #
     #
     # print('getMostCostlyFlow S1')

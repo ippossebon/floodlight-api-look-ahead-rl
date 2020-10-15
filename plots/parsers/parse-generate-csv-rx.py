95291f9c02e3e51f63162c28cab5ca778b54062e
Isadora <isadorapossebon@gmail.com>
2020-10-15 12:11:37 -0300

    wip

--- plots/parsers/parse-generate-csv-rx.py
+++ plots/parsers/parse-generate-csv-rx.py
@@ -1,42 +1,42 @@
 import csv
 
-txt_filename = '../data/output-app-15-set-1.csv'
 csv_filename = '../data/output-app-15-set-1_new.csv'
-
+new_filename = '../data/rx-values/15-set_new.csv'
 count = 0
 
 def parseLine(line):
     splitted_text = line.split(';')
-    step = splitted_text[0]
-    state = splitted_text[1]
-    reward_str = splitted_text[2]
+    state_str = splitted_text[1]
+    state_str = state_str.replace('[',  '')
+    state_str = state_str.replace(']', '')
+    state = state_str.split(' ')
 
-    reward = reward_str.replace('[', '')
-    reward = reward.replace(']', '')
-    reward = reward.replace(' ', '')
+    new_state = []
+    for value in state:
+        if value != '':
+            new_state.append(float(value.replace(',', '')))
 
-    return step, state, float(reward)
-
+    return new_state
 
 def main():
     input_lines = []
     count = 0
 
-    with open(txt_filename, 'r') as inputfile:
+    with open(csv_filename, 'r') as inputfile:
         lines = inputfile.readlines()
 
         for line in lines:
-            if count > 2:
+            if count > 0:
                 if line == '- - - - - - - - - - - - - - - - - - - - - - - - -\n':
                     break
 
-                step, state, reward = parseLine(line)
+                new_state = parseLine(line)
                 input_lines.append([step, state, reward])
             count += 1
 
     with open(csv_filename, 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
-        writer.writerow(['Step', 'State', 'Reward'])
+        writer.writerow(['S1.1', 'S1.2', 'S1.3', 'S2.1', 'S2.2', 'S2.3', 'S2.4', 'S3.1', 'S3.2', 'S3.3', 'S3.4', 'S4.1', 'S4.2', 'S4.3', 'S5.1', 'S5.2'])
 
         for line in input_lines:
             writer.writerow(line)

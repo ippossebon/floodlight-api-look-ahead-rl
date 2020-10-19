81fbcaa8622d0b40ff392a5b76a274439bd00f25
Isadora <isadorapossebon@gmail.com>
2020-10-18 22:03:10 -0300

    Add S10 topology

--- topologies/s10.py
+++ topologies/s10.py
@@ -16,12 +16,14 @@ class MastersSwitchTopo(Topo):
         s3 = self.addSwitch('S3', mac="00:00:00:00:00:03", protocols='OpenFlow13')
         s4 = self.addSwitch('S4', mac="00:00:00:00:00:04", protocols='OpenFlow13')
         s5 = self.addSwitch('S5', mac="00:00:00:00:00:05", protocols='OpenFlow13')
+        s6 = self.addSwitch('S6', mac="00:00:00:00:00:06", protocols='OpenFlow13')
+        s7 = self.addSwitch('S7', mac="00:00:00:00:00:07", protocols='OpenFlow13')
+        s8 = self.addSwitch('S8', mac="00:00:00:00:00:08", protocols='OpenFlow13')
+        s9 = self.addSwitch('S9', mac="00:00:00:00:00:09", protocols='OpenFlow13')
+        s10 = self.addSwitch('S10', mac="00:00:00:00:00:010", protocols='OpenFlow13')
 
         h1 = self.addHost('H1', mac="00:00:00:00:00:11", ip="10.0.0.1/12")
         h2 = self.addHost('H2', mac="00:00:00:00:00:12", ip="10.0.0.2/12")
-        h3 = self.addHost('H3', mac="00:00:00:00:00:13", ip="10.0.0.3/12")
-        h4 = self.addHost('H4', mac="00:00:00:00:00:14", ip="10.0.0.4/12")
-        h5 = self.addHost('H5', mac="00:00:00:00:00:15", ip="10.0.0.5/12")
 
 
         # Adiciona hosts aos switches
@@ -30,7 +32,7 @@ class MastersSwitchTopo(Topo):
  	        node2 = s1,
  	        port1 = 1,
  	        port2 = 1,
-            bw=10, # bw = Mbits/seg,
+            bw=50, # bw = Mbits/seg,
             cls=TCLink
         )
         self.addLink(
@@ -38,33 +40,10 @@ class MastersSwitchTopo(Topo):
  	        node2 = s3,
  	        port1 = 1,
  	        port2 = 1,
-            bw=10, # bw = Mbits/seg,
+            bw=50, # bw = Mbits/seg,
             cls=TCLink
         )
-        self.addLink(
-            node1 = h3,
- 	        node2 = s1,
- 	        port1 = 1,
- 	        port2 = 3,
-            bw=10, # bw = Mbits/seg,
-            cls=TCLink
-        )
-        self.addLink(
-            node1 = h4,
- 	        node2 = s1,
- 	        port1 = 1,
- 	        port2 = 4,
-            bw=10, # bw = Mbits/seg,
-            cls=TCLink
-        )
-        self.addLink(
-            node1 = h5,
- 	        node2 = s1,
- 	        port1 = 1,
- 	        port2 = 5,
-            bw=10, # bw = Mbits/seg,
-            cls=TCLink
-        )
+
 
         # Adiciona links entre os switches
         self.addLink(
@@ -78,7 +57,7 @@ class MastersSwitchTopo(Topo):
         self.addLink(
             node1 = s1,
             node2 = s4,
-            port1 = 6,
+            port1 = 3,
             port2 = 1,
             bw=10, # bw = Mbits/seg,
             cls=TCLink
@@ -124,12 +103,120 @@ class MastersSwitchTopo(Topo):
             cls=TCLink
         )
 
+        self.addLink(
+            node1 = s1,
+            node2 = s7,
+            port1 = 4,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s1,
+            node2 = s8,
+            port1 = 5,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s2,
+            node2 = s6,
+            port1 = 5,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s2,
+            node2 = s7,
+            port1 = 6,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s3,
+            node2 = s10,
+            port1 = 5,
+            port2 = 3,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s5,
+            node2 = s6,
+            port1 = 3,
+            port2 = 3,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s4,
+            node2 = s8,
+            port1 = 4,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s4,
+            node2 = s9,
+            port1 = 5,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s4,
+            node2 = s10,
+            port1 = 6,
+            port2 = 2,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s7,
+            node2 = s6,
+            port1 = 3,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s8,
+            node2 = s9,
+            port1 = 3,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
+        self.addLink(
+            node1 = s9,
+            node2 = s10,
+            port1 = 3,
+            port2 = 1,
+            bw=10, # bw = Mbits/seg,
+            cls=TCLink
+        )
+
 if __name__ == '__main__':
     setLogLevel('info')
     topo = MastersSwitchTopo()
     # c1 = RemoteController('c1', ip='127.0.0.1') #usando RYU
     # c1 = RemoteController('c1', ip='0.0.0.0', port=6653) #usando Floodlight
-    c1 = RemoteController('c1', ip='localhost', port=6653) #usando Floodlight
+    c1 = RemoteController('c1', ip='192.168.68.250', port=6653) #usando Floodlight
 
     net = Mininet(topo=topo, controller=c1)
     net.start()

c3dd1750f766cd4e73826875f0aaf719173dc2f3
Isadora <isadorapossebon@gmail.com>
2020-10-21 12:35:35 -0300

    wip

--- run-experiments.py
+++ run-experiments.py
@@ -88,7 +88,7 @@ def testAgent(env):
     model = DQN.load(load_path='./trained-agents/D1', env=env)
 
     state = env.reset()
-    num_steps = 500
+    num_steps = 700
 
     output_file_data = []
     output_file_data.append('Step; State; Reward')

f529ad749fdae1379bd0baa71dc35119e2a2344e
Isadora <isadorapossebon@gmail.com>
2020-10-24 19:54:33 -0300

    wip

--- scripts/add-initial-flow-entries.sh
+++ scripts/add-initial-flow-entries.sh
@@ -8,7 +8,17 @@ while read rulename switchid tcpsrc tcpdst outport
 do
   curl --location --request POST 'http://192.168.68.250:8080/wm/staticflowpusher/json' \
   --header 'Content-Type: application/json' \
-  -d "{\"name\": \"$rulename\",\"switch\": \"$switchid\",\"active\": \"true\",\"eth_type\": \"0x0800\",\"ipv4_src\": \"10.0.0.1\",\"ipv4_dst\": \"10.0.0.2\",\"ip_proto\": \"0x06\",\"tcp_src\": \"$tcpsrc\",\"tcp_dst\": \"$tcpdst\",\"actions\": \"output=$outport\"}"
+  -d "{
+      \"name\": \"$rulename\",
+      \"switch\": \"$switchid\",
+      \"active\": \"true\",
+      \"eth_type\": \"0x0800\",
+      \"ipv4_src\": \"10.0.0.1\",
+      \"ipv4_dst\": \"10.0.0.2\",
+      \"ip_proto\": \"0x06\",
+      \"tcp_src\": \"$tcpsrc\",
+      \"tcp_dst\": \"$tcpdst\"
+  }"
 
 done < $INPUT
 IFS=$OLDIFS

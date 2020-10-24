777a3ff39d3a8cb55a9c524cb4f55fd6bce1e130
Isadora <isadorapossebon@gmail.com>
2020-10-24 19:19:11 -0300

    wip

--- scripts/add-initial-flow-entries.sh
+++ scripts/add-initial-flow-entries.sh
@@ -1,19 +1,19 @@
 INPUT=initial_flow_entries.csv
 
-while IFS=, read -r rule_name, switch_id, tcp_src, tcp_dst, out_port
+while IFS=, read -r rulename, switchid, tcpsrc, tcpdst, outport
 do
   curl --location --request POST 'http://192.168.68.250:8080/wm/staticflowpusher/json' \
     --header 'Content-Type: application/json' \
     --data-raw '{
-      "name": "{{rule_name}}",
-      "switch": "{{switch_id}}",
+      "name": "{{rulename}}",
+      "switch": "{{switchid}}",
       "active": "true",
       "eth_type": "0x0800",
       "ipv4_src": "10.0.0.1",
       "ipv4_dst": "10.0.0.2",
       "ip_proto": "0x06",
-      "tcp_src": "{{tcp_src}}",
-      "tcp_dst": "{{tcp_dst}}",
-      "actions": "output={{out_port}}"
+      "tcp_src": "{{tcpsrc}}",
+      "tcp_dst": "{{tcpdst}}",
+      "actions": "output={{outport}}"
     }'
 done < $INPUT

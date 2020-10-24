62d2a67e7235ade2fb6ba65b986f2870a06a7b13
Isadora <isadorapossebon@gmail.com>
2020-10-24 19:27:55 -0300

    wip

--- scripts/add-initial-flow-entries.sh
+++ scripts/add-initial-flow-entries.sh
@@ -6,21 +6,27 @@ IFS=','
 
 while read rulename switchid tcpsrc tcpdst outport
 do
+  $RULENAME = rulename
+  $SWITCHID = switchid
+  $TCPSRC = tcpsrc
+  $TCPDST = tcpdst
+  $OUTPORT = outport
+
   wget --no-check-certificate \
     --method POST \
     --timeout=0 \
     --header 'Content-Type: application/json' \
     --body-data '{
-      "name": "{{rulename}}",
-      "switch": "{{switchid}}",
+      "name": $RULENAME,
+      "switch": $SWITCHID,
       "active": "true",
       "eth_type": "0x0800",
       "ipv4_src": "10.0.0.1",
       "ipv4_dst": "10.0.0.2",
       "ip_proto": "0x06",
-      "tcp_src": "{{tcpsrc}}",
-      "tcp_dst": "{{tcpdst}}",
-      "actions": "output={{outport}}"
+      "tcp_src": $TCPSRC,
+      "tcp_dst": $TCPDST,
+      "actions": $OUTPORT
   }' \
      'http://192.168.68.250:8080/wm/staticflowpusher/json'
 done < $INPUT

e81af1d54ea6ae67e705585d0ff613d59891422b
None <None>
2020-10-25 15:55:56 -0700

    wip

--- scripts/iperf-client.sh
+++ scripts/iperf-client.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]; then
-  echo "Expected 3 parameters, received $#"
-  echo "Usage: $0 <agent> <num_flows> <flow_size>"
+if [[ $# -ne 4 ]]; then
+  echo "Expected 4 parameters, received $#"
+  echo "Usage: $0 <agent> <num_flows> <flow_size> <replication>"
   exit 0
 fi
 
 agent=$1
 num_flows=$2
 flow_size=$3
-
-echo ""
+replication=$4
 
 for (( i=0; i < $num_flows; i++ )); do
   server_port=$(($i+5201))
   client_port=$(($i*2+46110))
 
-  iperf3 -c 10.0.0.2 -B 10.0.0.1 --cport $client_port -p $server_port -n $flow_size #> ~/floodlight-api-look-ahead-rl/output-experiments-iperfs/$agent-client-$client_port-$num_flows-$flow_size-v$i.log &
+  iperf3 -c 10.0.0.2 -B 10.0.0.1 --cport $client_port -p $server_port -n $flow_size > ~/floodlight-api-look-ahead-rl/output-experiments-iperfs/$agent-client-$client_port-$num_flows-flows-$flow_size-v$replication.log &
 done
 
 echo "Clients done"
--- scripts/start-iperfs.sh
+++ scripts/start-iperfs.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]; then
-  echo "Expected 3 parameters, received $#"
-  echo "Usage: $0 <agent> <num_flows> <flow_size>"
+if [[ $# -ne 4 ]]; then
+  echo "Expected 4 parameters, received $#"
+  echo "Usage: $0 <agent> <num_flows> <flow_size> <replication>"
   exit 0
 fi
 
 agent=$1
 num_flows=$2
 flow_size=$3
+replication=$4
 
-ssh mininet@192.168.68.250 "ssh -f 10.0.0.2 ~/floodlight-api-look-ahead-rl/scripts/iperf-server.sh $agent $num_flows $flow_size &"
-sleep 2
-ssh mininet@192.168.68.250 "ssh -f 10.0.0.1 ~/floodlight-api-look-ahead-rl/scripts/iperf-client.sh $agent $num_flows $flow_size &"
+# Start iperf3 servers
+
+for (( i=0; i < $num_flows; i++ )); do
+  port=$(($i+5201))
+  ssh -f 10.0.0.2 iperf3 -s -p $port -1 > ~/floodlight-api-look-ahead-rl/output-experiments-iperfs/$agent-server-$port-$num_flows-flows-$flow_size-v$replication.log
+done
+
+echo "Servers ready"
+
+sleep 5
+
+
+ssh -f 10.0.0.1 ~/floodlight-api-look-ahead-rl/scripts/iperf-client.sh $agent $num_flows $flow_size $replication

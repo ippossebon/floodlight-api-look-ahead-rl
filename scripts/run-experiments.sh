b121f4bdf47b6652958eec427798e3535e129603
Guilherme Alles <guilherme.r.alles@gmail.com>
2020-10-26 22:07:27 -0300

    fixes experiments script

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -14,7 +14,7 @@ chmod +x ./iperf-server.sh
 
 while read agent num_iperfs flow_size timesteps iter
 do
-	for (( i=0; i < $iter; i++ )); do
+	for (( i=0; i < 1; i++ )); do
     echo "Iniciando experimento: $agent - $num_iperfs iperfs - $flow_size - $timesteps steps - iteração $i"
 
     # Adiciona fluxos iniciais (python2)
@@ -22,41 +22,16 @@ do
 
     ./start-iperfs-server.sh $agent $num_iperfs $flow_size $iter
 
-    ./delayed-start-iperfs-client.sh $agent $num_iperfs $flow_size $iter
+    ./delayed-start-iperfs-client.sh $agent $num_iperfs $flow_size $iter &
 
-    # # Inicia agente
-    # CONTAINER_NAME='lookahead'
-    # # CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
-    # # if [ ! "${CID}" ]; then
-    #   # echo "Container doesn't exist"
-    #   # cd ..
-    #   # docker-compose up -d
-    #   # cd scripts
-    #   docker stop lookahead
-    #   docker rm lookahead
+    docker run -v $PWD/../:/app --network="bridge" lookahead python run-experiments.py -a $agent -n $num_iperfs -s $flow_size -t $timesteps -i $i
 
-
-    #   docker run -v $PWD/../:/app/ --network="bridge" --name="lookahead" -d lookahead python /app/run-experiments.py -a $agent -n $num_iperfs -s $flow_size -t $timesteps -i $i
-    # # fi
-    # unset CID
-
-    # docker exec -it lookahead "python /app/floodlight-api-look-ahead-rl/run-experiments.py -a $agent -n $num_iperfs -s $flow_size -t $timesteps >> /tmp/agent.log"
-    sleep 20
-    #
-    # # # Inicia iperfs
-    # echo "Iniciando iperfs..."
-    # ~/Documents/UFRGS/Mestrado/projeto/floodlight-api-look-ahead-rl/scripts/start-iperfs.sh $agent $num_iperfs $flow_size $i &
-
-    # echo "Aguardando finalização do agente e iperfs..."
-    # # Espera conclusão do agente e iperfs
-    # WAIT_TIME=$(($timesteps*7))
-    # sleep $WAIT_TIME
 
     echo "Removendo todas as entradas estáticas..."
     # Remove fluxos estaticos
     ./delete-flow-entries.sh
 
-    sleep 5
+    sleep 10
   done
 
 done < $INPUT

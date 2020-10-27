0c0454a11bf44b773791a44380c30fe30f42cf81
Isadora <isadorapossebon@gmail.com>
2020-10-27 13:15:12 -0300

    Refactor experiments settings

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT=experiments-config.csv
+INPUT=experiments-config-mvp.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
@@ -9,20 +9,18 @@ chmod +x ./add-initial-flow-entries.sh
 chmod +x ./kill-controller.sh
 chmod +x ./delete-flow-entries.sh
 chmod +x ./start-iperfs.sh
-chmod +x ./iperf-client.sh
-chmod +x ./iperf-server.sh
 
 while read agent num_iperfs flow_size timesteps iter
 do
-	for (( i=0; i < 1; i++ )); do
+	for (( i=0; i < $iter; i++ )); do
     echo "Iniciando experimento: $agent - $num_iperfs iperfs - $flow_size - $timesteps steps - iteração $i"
 
     # Adiciona fluxos iniciais (python2)
     ./add-initial-flow-entries.sh
 
-    ./start-iperfs-server.sh $agent $num_iperfs $flow_size $iter
+    ./start-iperfs-server.sh $agent $num_iperfs $flow_size $i
 
-    ./delayed-start-iperfs-client.sh $agent $num_iperfs $flow_size $iter &
+    ./delayed-start-iperfs-client.sh $agent $num_iperfs $flow_size $i &
 
     docker run -v $PWD/../:/app --network="bridge" lookahead python run-experiments.py -a $agent -n $num_iperfs -s $flow_size -t $timesteps -i $i
 

b4cf9a72d4a7dbbbd22cb691958f16aa52d9aacd
Isadora <isadorapossebon@gmail.com>
2021-01-14 22:54:48 -0300

    Add comp costs scripts

--- outputs-consolidated/generate-consolidated-compcosts-data.py
+++ outputs-consolidated/generate-consolidated-compcosts-data.py
@@ -12,7 +12,7 @@ MODEL, ORIGINAL_SIZE, TRANSFERRED, RETRIES, TIME, NUM_IPERFS
 
 OUTPUT_APP_DIR_NAME = '../output-experiments-app/'
 
-OUTPUT_FILENAME = './comp-costs-A-B-C-F.csv'
+OUTPUT_FILENAME = './data/comp-costs-A-B-C.csv'
 
 def parseFile(file_path):
 
@@ -23,44 +23,53 @@ def parseFile(file_path):
         time_splitted = time_string.split(':')
         time_hours = float(time_splitted[0])
         time_min = float(time_splitted[1])
+        time_sec_str = time_splitted[2].strip()
+        time_sec = float(time_sec_str)
+
         time_interval_min = (time_hours * 60) + time_min
+        time_interval_sec = (time_interval_min * 60) + time_sec
 
         memory_usage_kb = float(lines[1].strip())
 
-
-    return time_interval_min, memory_usage_kb
+    return time_interval_sec, memory_usage_kb
 
 
 def generateIperfCSVFile(agent, num_iperfs, flow_size):
-    dir_path = OUTPUT_IPERFS_DIR_NAME + '{0}-{1}iperfs-{2}/'.format(agent, num_iperfs, flow_size)
+    dir_path = OUTPUT_APP_DIR_NAME + '{0}-{1}iperfs-{2}/'.format(agent, num_iperfs, flow_size)
     files_in_dir = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
+    # import pdb; pdb.set_trace()
     for filename in files_in_dir:
         # Formato filename: A1-client-46112-2-flows-50M-v3.log
-        if filename != '.DS_Store' and 'compcosts' in filename:
+        if filename != '.DS_Store' and 'compcosts.txt' in filename:
             file_port = None
             file_iter = None
 
             is_client = 'client' in filename
             file_path = dir_path + filename
 
-            if is_client:
-                time_interval_min, memory_usage_kb = parseFile(file_path)
-                iter = int(file_iter_string.replace('v', ''))
+            filename_splitted = filename.split('-')
+            # ['A1', '2_flows', '10M', '15_steps', 'v_1', 'compcosts.txt']
+            num_iperfs_str = filename_splitted[1]
+            num_iperfs = int(num_iperfs_str.split('_')[0])
+            original_size = filename_splitted[2]
+            file_iter_string = filename_splitted[4]
+            iter = int(file_iter_string.split('_')[1])
 
+            time_interval_sec, memory_usage_kb = parseFile(file_path)
 
-                line = '{0},{1},{2},{3},{4},{5},{6}'.format(
-                    agent,
-                    num_iperfs,
-                    flow_size,
-                    time_interval_min,
-                    memory_usage_kb,
-                    iter
-                )
+            line = '{0},{1},{2},{3},{4},{5}'.format(
+                agent,
+                num_iperfs,
+                flow_size,
+                time_interval_sec,
+                memory_usage_kb,
+                iter
+            )
 
-                print('{0} --- {1}'.format(filename, line))
+            print('{0} --- {1}'.format(filename, line))
 
-                writeLineToFile(line, OUTPUT_FILENAME)
+            writeLineToFile(line, OUTPUT_FILENAME)
 
 
 def writeLineToFile(line, filename):
@@ -70,7 +79,7 @@ def writeLineToFile(line, filename):
 def main():
     experiments_config_file = './experiments-to-consolidate.csv'
 
-    # agent, num_iperfs, flow_size, time (min), memory (KB), iter
+    # agent, num_iperfs, flow_size, time (sec), memory (KB), iter
     header = 'agent, num_iperfs, flow_size, time, memory, iter'
     writeLineToFile(header, OUTPUT_FILENAME)
 

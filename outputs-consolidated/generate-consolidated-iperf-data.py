dbe48f4bc39c2e5ad78c1135beaa10576cdcad09
Isadora <isadorapossebon@gmail.com>
2021-01-25 19:50:12 -0300

    Add S2 consolidated experiments files

--- outputs-consolidated/generate-consolidated-iperf-data.py
+++ outputs-consolidated/generate-consolidated-iperf-data.py
@@ -12,7 +12,7 @@ MODEL, ORIGINAL_SIZE, TRANSFERRED, RETRIES, TIME, NUM_IPERFS
 
 OUTPUT_IPERFS_DIR_NAME = '../output-experiments-iperfs-raw/'
 
-OUTPUT_FILENAME = './data/iperfs-A-B-C-F.csv'
+OUTPUT_FILENAME = './data/iperfs-As2-Bs2-Fs2.csv'
 
 def parseClient(file_path):
     flow_completion_time = None

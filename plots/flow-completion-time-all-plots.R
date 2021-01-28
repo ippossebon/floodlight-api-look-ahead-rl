cbb786ca9dc733a811d5917e2892c0ffa51827ba
Isadora <isadorapossebon@gmail.com>
2021-01-28 19:20:51 -0300

    add files

--- plots/flow-completion-time-all-plots.R
+++ plots/flow-completion-time-all-plots.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-csvPath = '/Users/isadorapedrinipossebon/Documents/UFRGS/Mestrado/projeto/floodlight-api-look-ahead-rl/outputs-consolidated/data/iperfs-A-B-C-F.csv'
+csvPath = '/Users/isadorapedrinipossebon/Documents/UFRGS/Mestrado/projeto/floodlight-api-look-ahead-rl/outputs-consolidated/data/iperfs-As2-Bs2-Fs2.csv'
 
 data <- read_csv(csvPath, col_types = cols(
   agent = col_character(),

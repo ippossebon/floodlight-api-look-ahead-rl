668b2a5521c8a6e9c3a46b63fbd29e5b76df4790
Isadora <isadorapossebon@gmail.com>
2021-03-17 22:08:50 -0300

    add support for non matching flows

--- scripts/run-LA-experiments.sh
+++ scripts/run-LA-experiments.sh
@@ -11,11 +11,10 @@ chmod +x ./delete-flow-entries.sh
 chmod +x ./start-incremental-iperfs-server.sh
 chmod +x ./delayed-start-incremental-iperfs-client.sh
 
-flows_sizes=("25M" "50M" "100M" "200M" "400M" "800M" "1600M")
 
 while read agent iter
 do
-	for (( i=90; i < 95; i++ )); do
+	for (( i=0; i < $iter; i++ )); do
 	# for (( i=55; i < 56; i++ )); do
     echo "Iniciando experimento: $agent - iteração $i"
 
@@ -25,7 +24,7 @@ do
 
 	    ./delayed-start-incremental-iperfs-client.sh $agent $i &
 
-	    docker run -v $PWD/../:/app --network="bridge" lookahead python run-experiments.py -a $agent -n 9 -s ALL_FLOWS -t 4000 -i $i
+	    docker run -v $PWD/../:/app --network="bridge" lookahead python run-experiments.py -a $agent -n 8 -s ALL_FLOWS -t 5000 -i $i
 
 	    echo "Removendo todas as entradas estáticas..."
 
